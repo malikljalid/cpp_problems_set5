fe66c76713cccc156441760e3f655e87c6590d52
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-12-24 22:04:50 +0100

    stack implementation using the inhertance based on previous queue implementation

---
+++ project3_stackClass.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include "project2_queueClass.hpp"
+template <class data>
+
+class clsStack : public clsQueue <data>
+{
+    public:
+    void pop(void)
+    {
+        clsQueue <data>::_MyList.deleteLastNode();
+    }
+
+    data top(void)
+    {
+        return (clsQueue <data>::back());
+    }
+};
