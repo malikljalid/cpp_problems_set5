07c88bb2553c2be5bfcf3f4ba82470c50cd6b46e
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-12-24 17:34:28 +0100

    updating queue main program

--- project2_queueClass.cpp
+++ project2_queueClass.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "project2_queueClass.hpp"
 
 int main(void)
@@ -23,4 +22,39 @@ int main(void)
 
     std::cout << "\n\nQueue after pop() : \n";
     MyQueue.print();
+
+    //Extension #1
+    std::cout << "\n\n Item(2) : " << MyQueue.getItem(2);
+    
+    //Extension #2
+    MyQueue.reverse();
+    std::cout << "\n\nQueue after reverse() : \n";
+    MyQueue.print();
+
+    //Extension #3
+    MyQueue.updateItem(2, 600);
+    std::cout << "\n\nQueue after updating Item(2) to 600 : \n";
+    MyQueue.print();
+
+    //Extension #4
+    MyQueue.insertAfter(2, 800);
+    std::cout << "\n\nQueue after Inserting 800 after Item(2) : \n";
+    MyQueue.print();
+
+    //Extension #5
+    MyQueue.insertAtFront(1000);
+    std::cout << "\n\nQueue after Inserting 1000 at front: \n";
+    MyQueue.print();
+
+    //Extension #6
+    MyQueue.insertAtBack(2000);
+    std::cout << "\n\nQueue after Inserting 2000 at back: \n";
+    MyQueue.print();
+
+    //Extension #7
+    MyQueue.clear();
+    std::cout << "\n\nQueue after Clear(): \n";
+    MyQueue.print();
+
+    return (0);
 }
